2791a2a3cacfe2a98e7c991485536a4e63901730
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2022-10-19 22:41:55 +0700

    add update, delete, search recipe

--- infrastructure/router/gin.go
+++ infrastructure/router/gin.go
@@ -10,12 +10,9 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gsabadini/go-bank-transfer/adapter/api/action"
 	"github.com/gsabadini/go-bank-transfer/adapter/logger"
-	"github.com/gsabadini/go-bank-transfer/adapter/presenter"
 	"github.com/gsabadini/go-bank-transfer/adapter/repository"
 	"github.com/gsabadini/go-bank-transfer/adapter/validator"
-	"github.com/gsabadini/go-bank-transfer/usecase"
 )
 
 type ginEngine struct {
@@ -92,44 +89,9 @@ func (g ginEngine) setAppHandlers(router *gin.Engine) {
 
 	router.POST("/v1/recipes", g.NewRecipeHandler)
 	router.GET("/v1/recipes", g.ListRecipesHandler)
+	router.PUT("/v1/recipes/:id", g.UpdateRecipeHandler)
+	router.DELETE("/v1/recipes/:id", g.DeleteRecipeHandler)
+	router.GET("/v1/recipes/search", g.SearchRecipeHandler)
 
 	router.GET("/v1/health", g.healthcheck())
-}
-
-func (g ginEngine) buildCreateTransferAction() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var (
-			uc = usecase.NewCreateTransferInteractor(
-				repository.NewTransferNoSQL(g.db),
-				repository.NewAccountNoSQL(g.db),
-				presenter.NewCreateTransferPresenter(),
-				g.ctxTimeout,
-			)
-
-			act = action.NewCreateTransferAction(uc, g.log, g.validator)
-		)
-
-		act.Execute(c.Writer, c.Request)
-	}
-}
-
-func (g ginEngine) buildFindAllTransferAction() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var (
-			uc = usecase.NewFindAllTransferInteractor(
-				repository.NewTransferNoSQL(g.db),
-				presenter.NewFindAllTransferPresenter(),
-				g.ctxTimeout,
-			)
-			act = action.NewFindAllTransferAction(uc, g.log)
-		)
-
-		act.Execute(c.Writer, c.Request)
-	}
-}
-
-func (g ginEngine) healthcheck() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		action.HealthCheck(c.Writer, c.Request)
-	}
 }
--- infrastructure/router/recipe_handler.go
+++ infrastructure/router/recipe_handler.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -35,4 +36,89 @@ func (g ginEngine) NewRecipeHandler(c *gin.Context) {
 
 func (g ginEngine) ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+func (g ginEngine) UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe domain.Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe Not Found",
+		})
+		return
+	}
+
+	recipe.ID = id
+	recipes[index] = recipe
+
+	c.JSON(http.StatusOK, recipe)
+}
+
+func (g ginEngine) DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe domain.Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe Not Found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Deleted",
+	})
+}
+
+func (g ginEngine) SearchRecipeHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]domain.Recipe, 0)
+
+	for i := 0; i < len(recipes); i++ {
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				listOfRecipes = append(listOfRecipes, recipes[i])
+				c.JSON(http.StatusOK, listOfRecipes)
+				return
+			}
+
+		}
+
+	}
+
+	c.JSON(http.StatusOK, listOfRecipes)
+
 }
